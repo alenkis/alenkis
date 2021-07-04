9c8c113e83a2dd13c844c6a07be211380515e976
Alen <alen.kis@live.com>
2021-07-04 15:08:53 -0400

    change favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,7 +68,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/lambda.png`,
       },
     },
     `gatsby-plugin-react-helmet`,
