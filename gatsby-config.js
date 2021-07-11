0fcb71a42a39088845c29c58aa6150eb0bbc095d
Alen <alen.kis@live.com>
2021-07-10 21:43:34 -0400

    update google analyticS

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,14 +52,14 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
+    /*{
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: `G-QVPFR22ESE`,
         head: true,
         enableWebVitalsTracking: true,
       },
-    },
+    },*/
     `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
@@ -85,5 +85,30 @@ module.exports = {
     `gatsby-plugin-offline`,
     "gatsby-plugin-dark-mode",
     `gatsby-plugin-postcss`,
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-QVPFR22ESE", // Google Analytics
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        // gtagConfig: {
+        //  optimize_id: "OPT_CONTAINER_ID",
+        //  anonymize_ip: true,
+        //  cookie_expires: 0,
+        // },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          // respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          // exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
+      },
+    },
   ],
 }
